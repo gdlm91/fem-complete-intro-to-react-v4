57aa5b1d3ef757445fff2dc85113d89821d6faa8
Gerardo Leal <gerard.leal91@gmail.com>
2018-09-21 07:01:36 -0300

    5 - Async UI 1 (mine)

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,62 @@
-import React from 'react';
+import React from "react";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  state = {
+    loading: true
+  };
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+
+        // Clean-up breed data
+        let breed;
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+  }
   render() {
+    if (this.state.loading) {
+      return <h1>loading … </h1>;
+    }
+
+    const { animal, breed, location, description } = this.state;
+
     return (
-      <div>
-        <h1>Details page!</h1>
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+          <p>{description}</p>
+        </div>
       </div>
-    )
+    );
   }
 }
 
